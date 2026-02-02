f1eabd282ff9f9f7652be3e7e616faa3a7f88e3b
Kyle Gerner <kgerner@yext.com>
2026-02-02 18:24:53 -0500

    reference page template; remove ChatKit 'Widgets' usage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ReferencePage from "./ReferencePage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+function getReferenceFilename() {
+  const hash = window.location.hash;
+  if (!hash.startsWith("#reference")) return null;
+  const queryIndex = hash.indexOf("?");
+  const queryString = queryIndex >= 0 ? hash.slice(queryIndex + 1) : "";
+  const params = new URLSearchParams(queryString);
+  return params.get("filename");
+}
+
+function Root() {
+  const [referenceFilename, setReferenceFilename] = React.useState<string | null>(
+    getReferenceFilename()
+  );
+
+  React.useEffect(() => {
+    const onHashChange = () => setReferenceFilename(getReferenceFilename());
+    window.addEventListener("hashchange", onHashChange);
+    return () => window.removeEventListener("hashchange", onHashChange);
+  }, []);
+
+  if (referenceFilename !== null) {
+    return <ReferencePage filename={referenceFilename} />;
+  }
+
+  return <App />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Root />
+  </React.StrictMode>
+);
